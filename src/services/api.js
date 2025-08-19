599ede82413bded45d471414b9b8e3dbf01aab62
Dushyant Kumar <149196558+dushyant1222@users.noreply.github.com>
2025-08-20 03:31:10 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://backend-09bm.onrender.com/api',
   headers: { 'Content-Type': 'application/json' },
 });
 api.interceptors.request.use(config => {
