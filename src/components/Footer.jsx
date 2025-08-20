a4837cba7234ac32feda96157e74bbc91a1f3bb8
Dushyant Kumar <149196558+dushyant1222@users.noreply.github.com>
2025-08-21 00:34:05 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="bg-[#1a2335] text-white py-10">
       <div className="container mx-auto text-center text-sm">
-        &copy; 2025 TheSparkNova. All Rights Reserved.
+        &copy; 2025 TheSparkTank. All Rights Reserved.
       </div>
     </footer>
   );
