19decd33e585150e86ad611703e8f22f5d8a2f99
Dushyant Kumar <149196558+dushyant1222@users.noreply.github.com>
2025-08-21 15:29:21 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,6 +6,9 @@ export default function Footer() {
       <div className="container mx-auto text-center text-sm">
         &copy; 2025 TheSparkTank. All Rights Reserved.
       </div>
+      <a href="/privacy-policy">Privacy Policy</a> | 
+  <a href="/refund-policy">Refund Policy</a> | 
+  <a href="/terms-and-conditions">Terms & Conditions</a>
     </footer>
   );
 }
